56521f1a360ed9394ad36210856e6e031a9abedf
w <>
2020-08-19 12:45:53 -0400

    Footer Note refresh

--- src/components/FooterNote.js
+++ src/components/FooterNote.js
@@ -11,15 +11,18 @@ const FooterNote = ({ title }) => {
     <section className="section FooterNote">
       <div class="Footer--circles">
         <div class="Footer--circle"></div>
-        <div class="Footer--circle js-rellax" data-rellax-speed="1"></div>
-        <div class="Footer--circle js-rellax" data-rellax-speed="2"></div>
-        <div class="Footer--circle js-rellax" data-rellax-speed=".3"></div>
-        <div class="Footer--circle js-rellax" data-rellax-speed="2"></div>
-        <div class="Footer--circle js-rellax" data-rellax-speed="1"></div>
+        <div class="Footer--circle js-rellax"></div>
+        <div class="Footer--circle js-rellax"></div>
+        <div class="Footer--circle js-rellax"></div>
+        <div class="Footer--circle js-rellax"></div>
+        <div class="Footer--circle js-rellax"></div>
       </div>
       <div className="FooterNote-Container mx-auto">
         <div className="row mx-auto">
-          <div className="col-lg-7 col-md-6 col-10 taLeft my-auto">
+          <div className="col-lg-5 FooterNote--Img--Container d-block d-md-none d-lg-none">
+            <div className="FooterNote--Img"></div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-10 taLeft my-auto mx-auto">
             <h5>
               <Calendar />
             </h5>
@@ -31,7 +34,9 @@ const FooterNote = ({ title }) => {
             <Calendly url="https://calendly.com/centralavemarketing" />
           </div>
 
-          <div className="FooterNote--Img--Container"></div>
+          <div className="col-lg-5 FooterNote--Img--Container d-none d-md-block d-lg-block">
+            <div className="FooterNote--Img"></div>
+          </div>
         </div>
       </div>
     </section>
